61b05b9bd1d6b0fedd6eae286b43dd4f94dc1dba
Rodrigo <61331491+rodrigobergamindev@users.noreply.github.com>
2020-06-23 21:29:33 -0300

    commit

--- react-capital/src/components/Result.jsx
+++ react-capital/src/components/Result.jsx
@@ -7,31 +7,23 @@ export default function Result({quotas}) {
 
         return ( 
             <div className={css.result}>
-                    <table className={`striped ${css.tableResult}`}>
-                    <thead>
-                        <tr>
-                            <th>&nbsp;</th>
-                            <th>Capital</th>
-                            <th>Rendimento</th>
-                            <th>Juros</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
+                  {
                         quotas.map(quota => {
                         const {parcel,montant,valuePerMonth, rate} = quota
                             return (
-                                <tr key={parcel}>
-                                    <td style={{ fontWeight: 'bold' }}>{parcel}</td>
-                                    <td style={{ color: '#4caf50' }}>{formatMoney(montant)}</td>
-                                    {valuePerMonth >= 0 ? <td style={{ color: '#4caf50' }}>{'+' + formatMoney(valuePerMonth)}</td> : <td style={{ color: '#bf360c' }}>{formatMoney(valuePerMonth)}</td>}
-                                    {rate >= 0 ? <td style={{ color: '#0277bd' }}>{'+' + format(rate) + '%'}</td> : <td style={{ color: '#ff5722' }}>{format(rate) + '%'}</td>}
-                                </tr>
+                                <div className={css.data} key={parcel}>
+                                    <div className={css.header}>
+                                    <span style={{ fontWeight: 'bold', marginRight:'10px' }}>{parcel}</span>
+                                    </div>
+                                    <div className={css.content}> 
+                                    <span style={{ color: '#4caf50' }}>{formatMoney(montant)}</span>
+                                    {valuePerMonth >= 0 ? <span style={{ color: '#4caf50' }}>{'+' + formatMoney(valuePerMonth)}</span> : <span style={{ color: '#bf360c' }}>{formatMoney(valuePerMonth)}</span>}
+                                    {rate >= 0 ? <span style={{ color: '#0277bd' }}>{'+' + format(rate) + '%'}</span> : <span style={{ color: '#ff5722' }}>{format(rate) + '%'}</span>}
+                                    </div>
+                                </div>
                         )
                          })
                     }
-                    </tbody>
-                </table>
                </div>
     
         )
